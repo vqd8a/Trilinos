f51fc44358552ad23adef0a847a8405e53b20031
Vinh Dang <vqdang@sandia.gov>
2019-07-22 12:32:50 -0600

    Add tests

--- packages/pliris/test/vector_random/cxx_main.cpp
+++ packages/pliris/test/vector_random/cxx_main.cpp
@@ -41,32 +41,41 @@
 //@HEADER
 */
 
-
-
 #include <iostream>
-#include "Pliris.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sys/time.h>
+#include <mpi.h>
 
-#include "Epetra_MpiComm.h"
-#include "mpi.h"
+#include <Kokkos_Core.hpp>
+#include <Kokkos_Random.hpp>
+#include <KokkosBlas1_axpby.hpp>
+#include <KokkosBlas1_nrm2.hpp>
+#include <KokkosBlas2_gemv.hpp>
+#include "flops.h"
+#include "Pliris.hpp"
+#include "lin_solve.hpp"
 
-//  #include "Trilinos_Util.h"
-#ifndef __cplusplus
-#define __cplusplus
+#ifdef DREAL
+#define MPI_VALUE_TYPE MPI_DOUBLE 
 #endif
-#include "Epetra_SerialComm.h"
-#include "Epetra_Map.h"
-#include "Epetra_BlockMap.h"
-#include "Epetra_Time.h"
-#include "Epetra_MultiVector.h"
-#include "Epetra_Vector.h"
-#include "Epetra_LinearProblem.h"
-#include "Epetra_SerialDenseVector.h"
-
+#ifdef SREAL
+#define MPI_VALUE_TYPE MPI_FLOAT
+#endif
+#ifdef ZCPLX
+#define MPI_VALUE_TYPE MPI_DOUBLE_COMPLEX
+#endif
+#ifdef SCPLX
+#define MPI_VALUE_TYPE MPI_COMPLEX 
+#endif
 
 int main(int argc, char *argv[])
 {
-
-
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+  int rank, size;
+  
   int  my_rows;
   int  my_cols;
   int  my_first_row;
@@ -81,21 +90,14 @@ int main(int argc, char *argv[])
 
   MPI_Comm rowcomm;
 
-
   static int buf[4];
   int nrhs;
 
-  int i, m;
-  int k;
-  int l;
+  int i, m, k;
 
   int mlen;   // Message length for input data
 
-  int num_global_length;
-
   unsigned int seed= 10;
-
-  int num_my_length;
 
   double secs;
 
@@ -107,417 +109,269 @@ int main(int argc, char *argv[])
 
   double tempc;
 
-  double solinf;
-
-  double ainf;
-
-  double axbinf;
-
-  double xh;
-
-  double one =1.;
-
+  double rhs_nrm, m_nrm;
 
   // Enroll into MPI
 
-  MPI_Init(&argc,&argv);
-
-  Epetra_MpiComm comm(MPI_COMM_WORLD);
-
-
-  // Pliris example using the Epetra_Vector interface
-
+  MPI_Init(&argc,&argv);                             /* starts MPI */
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);             /* get current process id */
+  MPI_Comm_size (MPI_COMM_WORLD, &size);             /* get number of processes */
+  MPI_Get_processor_name(processor_name, &name_len); /* get name of the processor */
 
   // Initialize Input buffer
 
   for(i=0;i<4;i++) buf[i]=-1;
 
-  std::cout << "proc " << comm.MyPID() << " is alive of   " << comm.NumProc()<< " Processors" << std::endl ;
+  std::cout << "proc " << rank << " (" << processor_name << ") is alive of " << size << " Processors" << std::endl;
 
-  if( comm.MyPID() == 0 ) {
-
-
+  if( rank == 0 ) {
     // Check for commandline input
 
-     if (argc > 1) {
-       // argv[1] should be size of matrix
-       buf[0] = atoi(argv[1]);
-       if (argc > 2) {
-          // argv[2] should be #procs per row
-          buf[1] = atoi(argv[2]);
-       }
-       else
-          // default is 1, but sqrt(p) would be better
-          buf[1] = 1;
-     }
-     else {
+    if (argc > 1) {
+      // argv[1] should be size of matrix
+      buf[0] = atoi(argv[1]);
+      if (argc > 2) {
+        // argv[2] should be #procs per row
+        buf[1] = atoi(argv[2]);
+      }
+      else
+        // default is 1, but sqrt(p) would be better
+        buf[1] = 1;
+    }
+    else {
+      // Input Data about matrix and distribution
 
-    // Input Data about matrix and distribution
-
-   	if (buf[0] < 0) {
-	  std::cout << "Enter size of matrix " << std::endl;
-	  std::cin >> buf[0];
-	}
-	if (buf[1] < 0) {
-	  std::cout << "Enter number of processors to which each row is assigned "  << std::endl;
-	  std::cin >> buf[1];
-	}
-
-     }
-
-	std::cout << " Matrix Size "<< buf[0] <<"\n";
-
-        std::cout << " Processors in a row  " <<  buf[1] << "\n";
-
+      if (buf[0] < 0) {
+        std::cout << "Enter size of matrix " << std::endl;
+        std::cin >> buf[0];
+      }
+      if (buf[1] < 0) {
+        std::cout << "Enter number of processors to which each row is assigned "  << std::endl;
+        std::cin >> buf[1];
+      }
+    }
+    std::cout << " Matrix Size "<< buf[0] <<"\n";
+    std::cout << " Processors in a row  " <<  buf[1] << "\n";
   }
 
-   mlen = 4* sizeof(int);
+  /* Send the initilization data to each processor    */
+  mlen = 4*sizeof(int);
 
-    /* Send the initilization data to each processor    */
+  MPI_Bcast(reinterpret_cast<char *>(buf), mlen, MPI_CHAR, 0, MPI_COMM_WORLD);
 
-    // Using Epetra Communicator
+  // Set the values where needed
 
-    comm.Broadcast(buf,mlen,0);
+  matrix_size = buf[0];
 
+  nprocs_row  = buf[1];
 
-     // Set the values where needed
+  // Example for 1 RHS
 
-   matrix_size = buf[0];
+  nrhs = 1;
 
-   nprocs_row = buf[1];
-
-   // Example for 1 RHS
-
-   nrhs = 1;
-
-
-   if( comm.MyPID() == 0) {
+  if( rank == 0) {
     std::cout << " ---- Building Pliris solver ----" << std::endl;
-   }
-
-   // Instantiate the Solver
-
-
-   Pliris solver;
-
-   // Get Info to build the matrix on a processor
-
-   solver.GetDistribution( &nprocs_row,
-                            &matrix_size,
-			    &nrhs,
-                            &my_rows,
-                            &my_cols,
-                            &my_first_row,
-                            &my_first_col,
-                            &my_rhs,
-                            &my_row,
-			    &my_col );
-
-
-   //   Define a new communicator
-
-   MPI_Comm_split(MPI_COMM_WORLD,my_row,my_col,&rowcomm);
-
-   //if( comm.MyPID() == 0 ){
-   std::cout << " ------ PARALLEL Distribution Info for : ---------" <<std::endl;
-
-   std::cout << "   Processor  " << comm.MyPID() << std::endl
-        << "    my rows  " << my_rows << std::endl
-        << "    my cols  " << my_cols << std::endl
-        << "    my rhs  " << my_rhs << std::endl
-        << "    my first col  " << my_first_col  << std::endl
-        << "    my first row  " << my_first_row << std::endl
-        << "    my_row  " << my_row << std::endl
-        << "    num procs row   " << nprocs_row << std::endl
-        << "    my_col  " << my_col << std::endl;
-
-   //}
-
-   //  Local size -- my_rows  * (my_cols + my_rhs)
-
-
-   // Some temp arrays
-
-     double* temp = new double[my_rows];
-
-     double* temp2 = new double[my_rows];
-
-     double* rhs = new double[matrix_size];
-
-     double* temp4 = new double[matrix_size];
-
-
-     num_my_length = my_rows *(my_rhs + my_cols+6) ;
-
-
-     num_global_length = -1 ;
-
-
-     // Define Epetra_map
-
-
-     Epetra_Map map(num_global_length,num_my_length,
-  			 0, comm);
-
-
-      Epetra_Vector A(map);
-
-
-     // Set Random values
-
-     if( comm.MyPID() == 0 )
-              std::cout << " ****   Setting Random Matrix    ****" << std::endl;
-
-
-     A.SetSeed(seed+comm.MyPID() );
-
-     A.Random();
-
-
-     // Now Create the RHS
-
-
-      if( comm.MyPID() == 0 )
-              std::cout << " ****   Creating RHS   ****" << std::endl;
-
-     // Sum the portion of the row that I have
-
-
-      for (k= 0; k < my_rows; k++) {
-       temp[k] = 0.;
-       for (m=0; m < my_cols; m++) {
-        temp[k] = temp[k] + A[m*my_rows+k];
-       }
-     }
-
-    // Sum to Processor 0
-
-
-     MPI_Allreduce(temp,temp2,my_rows,MPI_DOUBLE,MPI_SUM,rowcomm);
-
-      if( comm.MyPID() == 0 )
-            std::cout << " ****   Packing RHS in Matrix   ****" << std::endl;
-
-    // Now put the RHS in the Appropriate position
-
-      for (k= 0; k < matrix_size; k++) {
-
-        temp4[k]=0.;
-        rhs[k]=0. ;
-      }
-
-    if( my_rhs > 0 ) {
-     for (k= 0; k < my_rows; k++) {
-
-      A[my_rows*my_cols + k] = temp2[k];
-
-      rhs[k+ my_first_row - 1]= temp2[k];
-
-     }
-    }
-
-     // Globally Sum the RHS needed for testing later
-
-
-     MPI_Allreduce(rhs,temp4,matrix_size,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
-
-     // Pack back into RHS
-
-     for (k=0 ; k < matrix_size; k++) {
-
-       rhs[k] = temp4[k];
-
-     }
-
-     delete [ ] temp4;
-
-
-    // Now Solve the Problem
-
-    if( comm.MyPID() == 0 )
-             std::cout << " ****   Beginning Matrix Solve   ****" << std::endl;
-
-     solver.FactorSolve(&A,
-		        my_rows,
-		        my_cols,
-                        &matrix_size,
-                        &nprocs_row,
-                        &nrhs,
-		        &secs);
-
-
-
-      if( comm.MyPID() == 0)   {
-         std::cout << " ----  Solution time  ----   "
-	 << secs << "  in secs. " << std::endl;
-
-
-	 mflops = 2./3.*pow(matrix_size,3.)/secs/1000000.;
-
-	   std::cout << " *****   MFLOPS   *****  " << mflops << std::endl;
-      }
-     // Now Check the Solution
-
-     // Delete temp variables they need to be resized
-
-     delete [] temp;
-
-     delete [] temp2;
-
-
-     temp = new double[matrix_size];
-
-     temp2 = new double[matrix_size];
-
-
-     // Intialize the Matrices
-
-      for (k= 0; k < matrix_size; k++) {
-
-	temp[k]=0. ;
-        temp2[k]=0. ;
-      }
-
-
-      // Pack the Answer into the apropriate position
-
-     if ( my_rhs > 0) {
-
-      for (k= 0; k < my_rows; k++) {
-
-       temp[k+ my_first_row -1] = A[my_rows*my_cols + k];
-
-      }
-     }
-
-
-     // All processors get the answer
-
-      MPI_Allreduce(temp,temp2,matrix_size,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
-
-      solinf = fabs(temp2[0]);
-
-      xh = fabs(temp2[0] -one);
-
-      for (k= 0; k < matrix_size; k++) {
-
-
-       if ( fabs (temp2[k]) > solinf ) solinf = fabs(temp2[k]);
-
-       if ( fabs (temp2[k]- one) > xh ) xh = fabs(temp2[k]-one);
-
-      }
-
-     // Reset the matrix Random values
-
-
-     A.SetSeed(seed + comm.MyPID() );
-
-
-     A.Random();
-
-
-     A.NormInf(&ainf);
-
-    // perform the Matrix vector product
-
-
-     for (k= 0; k < my_rows; k++) {
-      temp[k+my_first_row-1] = 0.;
-     for (l=0; l < my_cols; l++) {
-       temp[k+my_first_row-1] = temp[k+my_first_row-1] + A[l*my_rows+k]*temp2[l+my_first_col-1];
-     }
-    }
-
-
-     double * temp3 = new double[matrix_size];
-
-      for (k= 0; k < matrix_size; k++) {
-
-        temp3[k]=0. ;
-      }
-
-
-   //   Epetra_Map map(numGlobalEquations, numLocalEquations,
-   //			update, 0, comm);
-
-     MPI_Allreduce(temp,temp3,matrix_size,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
-
-
-     if( comm.MyPID() == 0)   {
-
-       std::cout <<  "======================================" << std::endl;
-        std::cout << " ---- Error Calculation ----" << std::endl;
-
-        axbinf = fabs(temp3[0]-rhs[0]);
-
-        for (k= 0; k < matrix_size; k++) {
+  }
+
+  // Get Info to build the matrix on a processor
+
+  Pliris_new::GetDistribution( &nprocs_row,
+                               &matrix_size,
+                               &nrhs,
+                               &my_rows,
+                               &my_cols,
+                               &my_first_row,
+                               &my_first_col,
+                               &my_rhs,
+                               &my_row,
+                               &my_col );
+
+  //   Define a new communicator
+
+  MPI_Comm_split(MPI_COMM_WORLD,my_row,my_col,&rowcomm);
+
+  std::cout << " ------ PARALLEL Distribution Info for : ---------" <<std::endl;
+
+  std::cout << "   Processor  " << rank << std::endl
+       << "    my rows  " << my_rows << std::endl
+       << "    my cols  " << my_cols << std::endl
+       << "    my rhs  " << my_rhs << std::endl
+       << "    my first col  " << my_first_col  << std::endl
+       << "    my first row  " << my_first_row << std::endl
+       << "    my_row  " << my_row << std::endl
+       << "    num procs row   " << nprocs_row << std::endl
+       << "    my_col  " << my_col << std::endl;
+
+  // Pliris example using the Kokkos Views
+  Kokkos::initialize( argc, argv );
+  {
+  //  Local size -- my_rows  * (my_cols + my_rhs)
+  
+  typedef Kokkos::LayoutLeft Layout;
+  typedef Kokkos::View<Kokkos::complex<double>**, Layout>  ViewMatrixType;
+  typedef Kokkos::View<Kokkos::complex<double>*,  Layout, Kokkos::HostSpace>  ViewVectorType_Host;
+  typedef typename ViewMatrixType::device_type::execution_space execution_space;
+  typedef typename ViewMatrixType::device_type::memory_space memory_space;
+  printf("Rank %d, ViewMatrixType execution_space %s, memory_space %s\n",rank,typeid(execution_space).name(),typeid(memory_space).name());
+
+  typedef typename ViewMatrixType::value_type ScalarA;
+
+  ViewMatrixType A( "A", my_rows, my_cols + my_rhs + 6 );
 	
-           if ( fabs (temp3[k]-rhs[k]) > axbinf ) axbinf = fabs(temp3[k]-rhs[k]);
+  ViewMatrixType::HostMirror h_A = Kokkos::create_mirror( A );
 
-         }
+  // Some temp arrays
 
-     }
+  ViewVectorType_Host temp  ( "temp", my_rows );
 
+  ViewVectorType_Host temp2 ( "temp2", my_rows );
 
-    // Machine epsilon Calculation
+  ViewVectorType_Host rhs   ( "rhs", matrix_size );
 
-      othird = four_thirds - 1.;
+  ViewVectorType_Host temp3 ( "temp3", matrix_size );
 
-      tempc = othird + othird + othird;
+  ViewVectorType_Host temp4 ( "temp4", matrix_size );
+  
+  ViewVectorType_Host tempp ( "tempp", matrix_size );
 
-      eps = fabs(tempc-1.0);
+  ViewVectorType_Host temp22( "temp22", matrix_size );
 
-      if ( comm.MyPID() == 0 ) {
+  // Set Random values
 
+  if( rank == 0 )
+    std::cout << " ****   Setting Random Matrix    ****" << std::endl;
+ 
+  Kokkos::Random_XorShift64_Pool<execution_space> rand_pool(seed+rank);
+  //Kokkos::fill_random(A, rand_pool,ScalarA(10));
+  Kokkos::fill_random(A, rand_pool,Kokkos::rand<Kokkos::Random_XorShift64<execution_space>,ScalarA >::max());
+
+  Kokkos::deep_copy( h_A, A );
+
+  // Now Create the RHS
+
+  if( rank == 0 )
+    std::cout << " ****   Creating RHS   ****" << std::endl;
+
+  // Sum the portion of the row that I have
+
+  for (k= 0; k < my_rows; k++) {
+    temp(k) = 0;
+    for (m=0; m < my_cols; m++) {
+     temp(k) = temp(k) + h_A(k,m);
+    }
+  }
+
+  // Sum to Processor 0
+
+  MPI_Allreduce(temp.data(), temp2.data(), my_rows, MPI_VALUE_TYPE, MPI_SUM, rowcomm);
+
+  if( rank == 0 )
+    std::cout << " ****   Packing RHS in Matrix   ****" << std::endl;
+
+  // Now put the RHS in the appropriate position
+
+  if( my_rhs > 0 ) {
+    Kokkos::deep_copy( subview(h_A,Kokkos::ALL(),my_cols), temp2 );
+    Kokkos::deep_copy( subview(rhs,Kokkos::make_pair(my_first_row - 1, my_first_row - 1 + my_rows)), temp2 );
+  }
+
+  // Globally Sum the RHS needed for testing later
+
+  MPI_Allreduce(rhs.data(), temp4.data(), matrix_size, MPI_VALUE_TYPE, MPI_SUM, MPI_COMM_WORLD);
+
+  // Pack back into RHS
+
+  Kokkos::deep_copy( rhs, temp4 );
+
+  rhs_nrm = KokkosBlas::nrm2(rhs);
+
+  Kokkos::deep_copy( subview(A,Kokkos::ALL(),my_cols), subview(h_A,Kokkos::ALL(),my_cols) );
+
+  // Now Solve the Problem
+
+  if( rank == 0 )
+    std::cout << " ****   Beginning Matrix Solve   ****" << std::endl;
+
+  linearAlgebra::lin_solve( A, my_rows, my_cols, matrix_size, nprocs_row, nrhs, &secs);
+
+  if( rank == 0) {
+    std::cout << " ----  Solution time  ----   " << secs << "  in secs. " << std::endl;
+
+    mflops = 2./3.*pow(matrix_size,3.)/secs/1000000.;
+
+    std::cout << " *****   MFLOPS   *****  " << mflops << std::endl;
+  }
+
+  // Now Check the Solution
+
+  Kokkos::deep_copy( subview(h_A,Kokkos::ALL(),my_cols), subview(A,Kokkos::ALL(),my_cols) );
+
+  // Pack the Answer into the apropriate position
+
+  if ( my_rhs > 0) {
+    Kokkos::deep_copy( subview(tempp,Kokkos::make_pair(my_first_row - 1, my_first_row - 1 + my_rows)), subview(h_A,Kokkos::ALL(),my_cols) );
+  }
+
+  // All processors get the answer
+
+  MPI_Allreduce(tempp.data(), temp22.data(), matrix_size, MPI_VALUE_TYPE, MPI_SUM, MPI_COMM_WORLD);
+  
+  // perform the Matrix vector product
+
+  ScalarA alpha = 1.0;
+  ScalarA beta  = 0.0;
+
+  KokkosBlas::gemv("N", alpha, subview(h_A,Kokkos::ALL(),Kokkos::make_pair(0, my_cols)),
+                               subview(temp22,Kokkos::make_pair(my_first_col - 1, my_first_col - 1 + my_cols)),
+                         beta, subview(tempp,Kokkos::make_pair(my_first_row - 1, my_first_row - 1 + my_rows)));
+	
+  MPI_Allreduce(tempp.data(), temp3.data(), matrix_size, MPI_VALUE_TYPE, MPI_SUM, MPI_COMM_WORLD);
+
+  if( rank == 0) {
+    std::cout <<  "======================================" << std::endl;
+    std::cout << " ---- Error Calculation ----" << std::endl;
+
+    ScalarA alpha_ = -1.0;
+
+    KokkosBlas::axpy(alpha_,rhs,temp3);//temp3=temp3-rhs
+
+    m_nrm = KokkosBlas::nrm2(temp3);
+  }
+
+  // Machine epsilon Calculation
+
+  othird = four_thirds - 1.;
+
+  tempc = othird + othird + othird;
+
+  eps = fabs(tempc-1.0);
+
+  if ( rank == 0 ) {
 	std::cout << "   Machine eps  " << eps  << std::endl;
+  }
 
-      }
+  if ( rank == 0 ) {
 
+    std::cout << "   ||Ax - b||_2 = " << m_nrm << std::endl;
 
+    std::cout << "   ||b||_2 = " << rhs_nrm << std::endl;
 
-     if ( comm.MyPID() == 0 ) {
+    std::cout << "   ||Ax - b||_2 / ||b||_2  = " << m_nrm/rhs_nrm  << std::endl;
 
+    std::cout << "   Threshold = " << eps*1e4  << std::endl;
 
+    if ( m_nrm/rhs_nrm  > (eps*1e4))
+      std::cout << " ****    Solution Fails   ****" <<  std::endl;
+    else
+      std::cout << " ****   Solution Passes   ****" << std::endl;
 
-       std::cout << "   ||Ax - b||_oo = " << axbinf << std::endl;
+    std::cout <<  "======================================" << std::endl;
+  }
 
-       std::cout << "   ||A||_oo ~  " << ainf*matrix_size << std::endl;
-
-       std::cout << "   ||X||_oo = " << solinf << std::endl;
-
-       std::cout << "   ||Ax - b||_oo / ||A||_oo/||X||_oo  = " << axbinf/(ainf*matrix_size)/solinf  << std::endl;
-
-       std::cout << "  matrix_size*eps*1.  " << matrix_size*eps*1. << std::endl;
-
-      if ( axbinf/(ainf*matrix_size)/solinf  > (matrix_size*eps*1.))
-
-        std::cout << " ****    Solution Fails   ****" <<  std::endl;
-
-      else
-
-	std::cout << " ****   Solution Passes   ****" << std::endl;
-
-      std::cout <<  "======================================" << std::endl;
-
-
-     }
-
-
-     //  Delete all the vectors used
-
-
-     delete [] temp2;
-
-     delete [] temp;
-
-     delete [] rhs;
-
-     delete [] temp3;
-
-
+  }
+  Kokkos::finalize();
 
   MPI_Finalize() ;
 
-return (0);
+  return (0);
 }
