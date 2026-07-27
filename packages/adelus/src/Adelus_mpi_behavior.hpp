f698e5830266d5bba54dfa3e33a2760aad6f35d2
Vinh Dang <vqdang@sandia.gov>
2026-07-27 12:53:26 -0700

    Add function to probe the env variable
    Signed-off-by: Vinh Dang <vqdang@sandia.gov>

---
+++ packages/adelus/src/Adelus_mpi_behavior.hpp
@@ -0,0 +1,40 @@
+/*
+//@HEADER
+// *****************************************************************************
+//                        Adelus
+//
+// Copyright 2020 NTESS and the Adelus contributors.
+// SPDX-License-Identifier: BSD-3-Clause
+// *****************************************************************************
+//@HEADER
+*/
+
+#ifndef __ADELUS_MPI_BEHAVIOR_HPP__
+#define __ADELUS_MPI_BEHAVIOR_HPP__
+
+#include "Teuchos_EnvVariables.hpp"
+
+namespace Adelus {
+
+constexpr const std::string_view USE_GPU_AWARE_MPI_ENV_VAR = "ADELUS_USE_GPU_AWARE_MPI";
+
+constexpr bool MpiIsGPUAwareDefault() {
+#ifdef ADELUS_ENABLE_GPU_AWARE_MPI
+    return true;
+#else
+    return false;
+#endif  // USE_GPU_AWARE_MPI
+}
+
+bool probeMpiIsGPUAware() {
+  constexpr bool defaultValue = MpiIsGPUAwareDefault();
+
+  static bool value_       = defaultValue;
+  static bool initialized_ = false;
+  return Teuchos::idempotentlyGetEnvironmentVariable(value_, initialized_, Adelus::USE_GPU_AWARE_MPI_ENV_VAR, defaultValue);
+}
+
+}
+
+#endif
+
